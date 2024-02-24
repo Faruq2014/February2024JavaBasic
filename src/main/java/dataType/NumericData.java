1ebbad5ca8b23879f5cf2387235a2e2c66fe4842
Mohammed Molla <mjmolla@gmail.com>
2024-02-24 13:03:38 -0500

    initial

---
+++ src/main/java/dataType/NumericData.java
@@ -0,0 +1,17 @@
+package dataType;
+
+public class NumericData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		System.out.println("Hello Java");
+			
+	//first brace==()== prethisis
+		//second brace={}== carly brace
+		//third brace=[]== sqare brace
+
+	}
+
+}
